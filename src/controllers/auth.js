8ed272f69d4e8b658179555196522244c4cd0ebf
a-nester <a_nester@ukr.net>
2024-08-16 19:45:01 +0300

    userLogin

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,16 @@
-import { registerUser } from "../services/auth.js";
+import { loginUser, registerUser } from "../services/auth.js";
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: "Successfully registered a user!",
+    data: user,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const user = await loginUser(req.body);
+  // return user;
 };
